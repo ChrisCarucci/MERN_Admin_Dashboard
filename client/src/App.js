113ccfff4689e82e1d2fa56f9b9d14c4b2fcfbc6
ChrisCarucci <Chris.w.carucci@gmail.com>
2023-01-23 01:40:58 -0330

    Daily + Monthly Charts

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,7 @@ import Customers from "scenes/customers";
 import Transactions from "scenes/transactions";
 import Geography from "scenes/geography";
 import Overview from "scenes/overview.jsx";
+import Daily from "scenes/daily.js";
 
 
 
@@ -32,6 +33,8 @@ function App() {
               <Route path="/transactions" element={<Transactions />} />
               <Route path="/geography" element={<Geography />} />
               <Route path="/overview" element={<Overview />} />
+              <Route path="/daily" element={<Daily />} />
+              <Route path="/monthly" element={<Monthly />} />
             </Route>
           </Routes>
         </ThemeProvider>
